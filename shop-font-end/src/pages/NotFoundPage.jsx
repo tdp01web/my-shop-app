7ac3940bae97f2123bf0786ed4f345b27f9cc831
th120369 <tienhoangxs122333@gmail.com>
2024-07-27 15:29:08 +0700

    fix notfound-page

--- shop-font-end/src/pages/NotFoundPage.jsx
+++ shop-font-end/src/pages/NotFoundPage.jsx
@@ -9,15 +9,15 @@ const NotFoundPage = () => {
       <div className="max-w-screen-xl mx-auto bg-[#ECECEC]">
         <HeaderLayoutClient/>
         <main className="w-full md:w-[60%] mx-auto">
-          <div className="w-full py-8 px-4 mx-auto bg-white">
-            <div className="mx-auto max-w-screen-sm text-center">
+          <div className=" bg-white">
+            <div className="text-center">
               <img src="https://file.hstatic.net/200000636033/file/404_0031c39563584f53bce84ce2e6add746.png" alt="" />
-              <p className="mb-4 text-xl tracking-tight font-bold text-gray-900 md:text-4xl">
+              <h1 className="mb-4 text-xl tracking-tight font-bold text-gray-900 md:text-4xl">
                 Rất tiếc trang bạn tìm kiếm đang không tồn tại
-              </p>
-              <p className="text-sm font-light text-gray-500 md:text-lg">
+              </h1>
+              <h2 className="text-sm font-light text-gray-500 md:text-lg">
                 Nếu bạn cần hỗ trợ, vui lòng liên hệ tổng đài 
-              </p>
+              </h2>
               <p className="text-lg font-light text-red-900">
               1800 6975
               </p>
