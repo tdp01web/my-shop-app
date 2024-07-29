a85cfc55929e10e2c0c3402c361e285301f97f20
hocclnh <hocntph22459@fpt.edu.vn>
2024-07-29 22:15:32 +0700

    route new

--- shop-font-end/src/App.jsx
+++ shop-font-end/src/App.jsx
@@ -1,6 +1,5 @@
 import Router from "./routes";
 import "./styles/App.css";
-import { Button } from 'antd';
 function App() {
   return (
     <>
---
+++ shop-font-end/src/pages/client/OrdersPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OrdersPage = () => {
+  return (
+    <div>trang đơn hàng của tôi</div>
+  )
+}
+
+export default OrdersPage
