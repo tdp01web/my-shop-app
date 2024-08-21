3ba76637a6fce74a6b588bea172171feed80c550
Tien Hoang <thoang12@innofly-software.com>
2024-08-21 14:47:55 +0700

    add loading components

---
+++ shop-font-end/src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="loader">
+      <p className="text">
+        loading...
+      </p>
+    </div>
+  )
+}
+
+export default Loading
