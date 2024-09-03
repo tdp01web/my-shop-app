50a8e1f8705403e2c3cc044a5f7c17f9815e8498
Phanh <anhntpph37065@fpt.edu.vn>
2024-09-03 08:04:52 +0700

    fix not-search

--- shop-font-end/src/pages/NotFoundSearch.jsx
+++ shop-font-end/src/pages/NotFoundSearch.jsx
@@ -1,3 +1,4 @@
+import  { useState } from 'react';
 import FooterLayoutClient from "../layouts/client/components/footer";
 import HeaderLayoutClient from "../layouts/client/components/header";
 import { Typography, Input, Button, Card, Layout, Form } from 'antd';
@@ -9,14 +10,36 @@ const { Title, Paragraph } = Typography;
 const { Content } = Layout;
 
 const NotFoundSearch = () => {
-  const onFinish = (values) => {
-    console.log('Thêm thành công', values);
+  const [inputValue, setInputValue] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [searchResult, setSearchResult] = useState(false);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    if (e.target.value) {
+      setErrorMessage('');
+      setSearchResult(false); 
+    } else {
+      setErrorMessage('Trường tìm kiếm không được để trống !');
+      setSearchResult(false);
+    }
+  };
+
+  const onFinish = () => {
+    if (!inputValue) {
+      setErrorMessage('Trường tìm kiếm không được để trống !');
+      setSearchResult(false);
+    } else {
+      setErrorMessage('');
+      setSearchResult(true); 
+      console.log('Thêm thành công', inputValue);
+    }
   };
 
   return (
     <div className="bg-[#ECECEC] mx-auto max-w-screen-xl">
       <HeaderLayoutClient />
-      <Banner/>
+      <Banner />
       <Content className="p-4 sm:p-8 lg:p-12 text-center">
         <Card className="w-full max-w-screen-lg mx-auto p-4 sm:p-6 lg:p-8" bordered={false}>
           <Title className="text-2xl sm:text-3xl lg:text-2xl">TÌM KIẾM</Title>
@@ -28,8 +51,12 @@ const NotFoundSearch = () => {
 
           <div className="flex justify-center mt-4 sm:mt-5">
             <Form name="basic" className="flex flex-col sm:flex-row items-center" onFinish={onFinish} autoComplete="off">
-              <Form.Item name="title" rules={[{ required: true, message: 'Vui lòng điền từ khóa' }]} className="mb-2 sm:mb-0">
-                <Input defaultValue="bim bim" className="w-full sm:w-[300px] lg:w-[400px] h-[40px] sm:h-[50px] border border-black align-middle" />
+              <Form.Item className="mb-2 sm:mb-0">
+                <Input 
+                  value={inputValue}
+                  onChange={handleInputChange}
+                  className="w-full sm:w-[300px] lg:w-[400px] h-[40px] sm:h-[50px] border border-black align-middle"
+                />
               </Form.Item>
               <Form.Item className="mb-0">
                 <Button type="primary" htmlType="submit" className="bg-black border border-black w-full sm:w-[100px] h-[40px] sm:h-[50px] text-white align-middle mt-2 sm:mt-0 sm:ml-2">
@@ -38,12 +65,23 @@ const NotFoundSearch = () => {
               </Form.Item>
             </Form>
           </div>
-          <Paragraph className="line-clamp-2 font-semibold text-[16px] sm:text-[18px] mt-4 sm:mt-6 lg:mt-7">
-            Rất tiếc, chúng tôi không thể tìm thấy từ khóa của bạn
-          </Paragraph>
-          <Paragraph className="line-clamp-2 font-semibold text-[16px] sm:text-[18px]">
-            Vui lòng kiểm tra chính tả, sử dụng các từ khóa khác và thử lại!
-          </Paragraph>
+
+          {errorMessage && (
+            <Paragraph className="text-red-500 font-semibold text-[16px] sm:text-[18px] mt-4">
+              {errorMessage}
+            </Paragraph>
+          )}
+          
+          {searchResult && (
+            <>
+              <Paragraph className="line-clamp-2 font-semibold text-[16px] sm:text-[18px] mt-4 sm:mt-6 lg:mt-7">
+                Rất tiếc, chúng tôi không thể tìm thấy từ khóa của bạn.
+              </Paragraph>
+              <Paragraph className="line-clamp-2 font-semibold text-[16px] sm:text-[18px]">
+                Vui lòng kiểm tra chính tả, sử dụng các từ khóa khác và thử lại!
+              </Paragraph>
+            </>
+          )}
         </Card>
       </Content>
       <FooterLayoutClient />
