cc8b496125b0c11f2e85b87cd61bf5139e6fab91
namnm204 <nammmmmm008@gmail.com>
2024-11-09 10:58:55 +0700

    fix code tìm kiếm

--- shop-font-end/src/layouts/client/components/header/component/Search.jsx
+++ shop-font-end/src/layouts/client/components/header/component/Search.jsx
@@ -8,47 +8,62 @@ import { instance } from "./../../../../../configs/instance";
 
 const SearchProduct = () => {
   const [query, setQuery] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
   const fetchProducts = async (query) => {
-    const response = await instance.get(`/product/search?query=${query}`);
-    return response.data;
+    try {
+      const response = await instance.get(`/product/search?query=${query}`);
+      if (response.data.message === "Không tìm thấy sản phẩm nào.") {
+        return null;
+      }
+      return response.data;
+    } catch (error) {
+      console.error("Lỗi khi tìm kiếm sản phẩm:", error);
+      return null;
+    }
   };
 
   const handleSearch = async (e) => {
     e.preventDefault();
+    setErrorMessage("");
     if (query.length > 0) {
       const results = await fetchProducts(query);
-      console.log(results);
-      navigate("/search", { state: { query, results } });
+      if (results) {
+        navigate("/search", { state: { query, results } });
+      } else {
+        setErrorMessage("Không tìm thấy sản phẩm nào.");
+        navigate("/not-found-search", { state: { query } });
+      }
     }
   };
 
   return (
     <div className="w-[60%] sm:w-[80%] md:w-[50%] 2xl:w-[35%]">
-      <div className=" w-[40%] sm:w-[70%] md:w-[50%] 2xl:w-[35%]">
-        <Paper
-          component="form"
-          onSubmit={handleSearch}
-          sx={{
-            p: "2px 4px",
-            display: "flex",
-            alignItems: "center",
-            width: "100%",
-            height: 40,
-          }}
-        >
-          <InputBase
-            sx={{ ml: 1, flex: 1 }}
-            placeholder="Bạn muốn tìm gì"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-            <SearchIcon />
-          </IconButton>
-        </Paper>
-      </div>
+      <Paper
+        component="form"
+        onSubmit={handleSearch}
+        sx={{
+          p: "2px 4px",
+          display: "flex",
+          alignItems: "center",
+          width: "100%",
+          height: 40,
+        }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Bạn muốn tìm gì"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      {errorMessage && (
+        <p style={{ color: "red", marginTop: "10px" }}>{errorMessage}</p>
+      )}
     </div>
   );
 };
