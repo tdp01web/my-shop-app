7d607b2f7b52a85037c05bd1105869a4c1f61ecc
Bùi Xuân Đạt <150761490+BuiDat2012@users.noreply.github.com>
2024-11-21 08:55:03 +0700

    update ui

--- shop-font-end/src/pages/client/UpdatePassword/index.jsx
+++ shop-font-end/src/pages/client/UpdatePassword/index.jsx
@@ -65,83 +65,125 @@ const UpdatePassword = () => {
   };
 
   return (
-    <div className="flex flex-col gap-3 p-5 justify-center items-center">
-      <Title level={3}>Đổi mật khẩu</Title>
-      <div className="flex flex-col gap-3 w-[40%]">
-       <div>
-          <p className="mb-[4px]">
-            <span className="text-[14px]">Mật khẩu hiện tại</span>
-            <span className="text-red-500"> *</span>
-          </p>
-          <Input.Password
-            placeholder="Mật khẩu hiện tại"
-            value={currentPassword}
-            onChange={(e) => setCurrentPassword(e.target.value)}
-            status={error.currentPassword ? "error" : ""}
-          />
-       </div>
+    <>
+      <h2 className="text-[24px] font-semibold px-6 py-4 text-[#333] leading-tight">
+        Đổi mật khẩu
+      </h2>
 
-        <div>
-          <p className="mb-[4px]">
-            <span className="text-[14px]">Mật khẩu mới</span>
-            <span className="text-red-500"> *</span>
-          </p>
+      <div className="px-6 py-4 max-w-[580px]">
+        <div className="flex flex-col gap-3">
+          <div className="grid grid-cols-12 items-center gap-[8px]">
+            <p className="mb-[4px] col-span-4 text-right">
+              <span className="text-red-500">* </span>
+              <span className="text-[16px]">Mật khẩu hiện tại: </span>
+            </p>
 
-          <Input.Password
-            placeholder="Mật khẩu mới"
-            value={newPassword}
-            onChange={(e) => {
-              setNewPassword(e.target.value);
-              setError({
-                ...error,
-                newPassword:
-                  e.target.value.length < 8
-                    ? "Mật khẩu mới phải có ít nhất 8 ký tự!"
-                    : "",
-              });
-            }}
-            status={error.newPassword ? "error" : ""}
-          />
-          {error.newPassword && <Text type="danger">{error.newPassword}</Text>}
+            <Input.Password
+              placeholder="Mật khẩu hiện tại"
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+              status={error.currentPassword ? "error" : ""}
+              size="large"
+              className="rounded-[4px] col-span-8"
+            />
+          </div>
+
+          <div>
+            <div className="grid grid-cols-12 items-center gap-[8px]">
+              <p className="mb-[4px] col-span-4 text-right">
+                <span className="text-red-500">* </span>
+                <span className="text-[16px]">Mật khẩu mới: </span>
+              </p>
+
+              <div className="col-span-8">
+                <Input.Password
+                  placeholder="Mật khẩu mới"
+                  value={newPassword}
+                  onChange={(e) => {
+                    setNewPassword(e.target.value);
+                    setError({
+                      ...error,
+                      newPassword:
+                        e.target.value.length < 8
+                          ? "Mật khẩu mới phải có ít nhất 8 ký tự!"
+                          : "",
+                    });
+                  }}
+                  status={error.newPassword ? "error" : ""}
+                  size="large"
+                  className="rounded-[4px]"
+                />
+              </div>
+            </div>
+
+            <div className="grid grid-cols-12 gap-[8px]">
+              <div className="col-span-4"></div>
+
+              <div className="col-span-8">
+                {error.newPassword && (
+                  <Text type="danger">{error.newPassword}</Text>
+                )}
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="grid grid-cols-12 items-center gap-[8px]">
+              <p className="mb-[4px] col-span-4 text-right">
+                <span className="text-red-500">* </span>
+                <span className="text-[16px]">Xác nhận mật khẩu:</span>
+              </p>
+
+              <div className="col-span-8">
+                <Input.Password
+                  placeholder="Xác nhận mật khẩu mới"
+                  value={confirmPassword}
+                  onChange={(e) => {
+                    setConfirmPassword(e.target.value);
+                    setError({
+                      ...error,
+                      confirmPassword:
+                        e.target.value !== newPassword
+                          ? "Mật khẩu xác nhận không khớp!"
+                          : "",
+                    });
+                  }}
+                  size="large"
+                  className="rounded-[4px]"
+                  status={error.confirmPassword ? "error" : ""}
+                />
+              </div>
+            </div>
+
+            <div className="grid grid-cols-12 gap-[8px]">
+              <div className="col-span-4"></div>
+              <div className="col-span-8">
+                {error.confirmPassword && (
+                  <Text type="danger">{error.confirmPassword}</Text>
+                )}
+              </div>
+            </div>
+          </div>
+
+          {error.form && <Text type="danger">{error.form}</Text>}
+
+          <div className="grid grid-cols-12 gap-[8px]">
+            <div className="col-span-4"></div>
+            <div className="col-span-8">
+              <Button
+                type="primary"
+                className="bg-red-700 rounded-[4px]"
+                onClick={handleSubmit}
+                size="large"
+                loading={mutation.isLoading}
+              >
+                Đổi mật khẩu
+              </Button>
+            </div>
+          </div>
         </div>
-
-        <div>
-          <p className="mb-[4px]">
-            <span className="text-[14px]">Xác nhận mật khẩu</span>
-            <span className="text-red-500"> *</span>
-          </p>
-
-          <Input.Password
-            placeholder="Xác nhận mật khẩu mới"
-            value={confirmPassword}
-            onChange={(e) => {
-              setConfirmPassword(e.target.value);
-              setError({
-                ...error,
-                confirmPassword:
-                  e.target.value !== newPassword
-                    ? "Mật khẩu xác nhận không khớp!"
-                    : "",
-              });
-            }}
-            status={error.confirmPassword ? "error" : ""}
-          />
-          {error.confirmPassword && (
-            <Text type="danger">{error.confirmPassword}</Text>
-          )}
-        </div>
-
-        {error.form && <Text type="danger">{error.form}</Text>}
-        <Button
-          type="primary"
-          className="bg-red-700"
-          onClick={handleSubmit}
-          loading={mutation.isLoading}
-        >
-          Đổi mật khẩu
-        </Button>
       </div>
-    </div>
+    </>
   );
 };
 
