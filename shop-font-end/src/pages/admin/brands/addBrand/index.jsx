79ebc564380c5c4afb6fd7e9a3f63e26c420f8a4
Nguyen-thanh844 <thanhntph38561@fpt.edu.vn>
2025-01-12 18:21:43 +0700

    call api post brand

--- shop-font-end/src/pages/admin/brands/addBrand/index.jsx
+++ shop-font-end/src/pages/admin/brands/addBrand/index.jsx
@@ -1,13 +1,51 @@
-import { BackwardFilled } from "@ant-design/icons";
-import { Button, Form, Input } from "antd";
-import { Link } from "react-router-dom";
+import { BackwardFilled, Loading3QuartersOutlined, PlusOutlined } from "@ant-design/icons";
+import { Button, Form, Input, InputNumber, message, Select, Upload } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import { usePostBrand } from "../../../../hooks/mutations/usePostBrand";
 
 const AddBrands = () => {
+  const navigate = useNavigate()
+  const [messageApi, contextHolder] = message.useMessage();
+  const [form] = Form.useForm();
+  const { mutate, isPending } = usePostBrand({
+    onSuccess: () => {
+      messageApi.open({
+        type: "success",
+        content: "Thêm hãng thành công",
+      });
+      setTimeout(() => {
+        navigate('/admin/brands');
+      }, 1000);
+      form.resetFields();
+    },
+    onError(error) {
+      if (error.response?.data) {
+        const { message } = error.response.data;
+        if (message.includes("duplicate key error")) {
+          messageApi.open({
+            type: "error",
+            content: "Hãng đã tồn tại. Vui lòng chọn tên khác.",
+          });
+        } else {
+          messageApi.open({
+            type: "error",
+            content: message,
+          });
+        }
+      } else {
+        messageApi.open({
+          type: "error",
+          content: "Đã xảy ra lỗi. Vui lòng thử lại.",
+        });
+      }
+    },
+  });
   const onFinish = (values) => {
-    console.log(values);
+    mutate(values);
   };
   return (
     <div className="">
+      {contextHolder}
       <div className="flex justify-between items-center mb-5">
         <h1 className="font-semibold text-2xl">Thêm hãng</h1>
         <Button type="primary">
@@ -18,12 +56,14 @@ const AddBrands = () => {
       </div>
       <div className="mx-auto max-w-3xl">
         <Form
+          form={form}
           name="basic"
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 16 }}
           style={{ maxWidth: 600 }}
           onFinish={onFinish}
           autoComplete="off"
+          disabled={isPending}
         >
           <Form.Item
             label="Tên hãng"
@@ -36,8 +76,15 @@ const AddBrands = () => {
             <Input placeholder="Nhập tên hãng" />
           </Form.Item>
           <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit" >
-              Cập nhật
+            <Button type="primary" htmlType="submit" disabled={isPending}>
+              {isPending ? (
+                <>
+                  <Loading3QuartersOutlined className="mr-2 animate-spin" />
+                  Cập nhật
+                </>
+              ) : (
+                "Cập nhật"
+              )}
             </Button>
           </Form.Item>
         </Form>
