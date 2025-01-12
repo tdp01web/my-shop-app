6c8a5c4614852ce17159336a61dc7a864aa9c70e
Nguyen-thanh844 <thanhntph38561@fpt.edu.vn>
2025-01-12 18:20:38 +0700

    call api list brand and delete brand

--- shop-font-end/src/pages/admin/brands/index.jsx
+++ shop-font-end/src/pages/admin/brands/index.jsx
@@ -1,14 +1,42 @@
 import React, { useRef, useState } from "react";
 import { PlusCircleFilled, SearchOutlined } from "@ant-design/icons";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Button, Input, message, Popconfirm, Space, Table } from "antd";
 import Highlighter from "react-highlight-words";
 import { Link } from "react-router-dom";
+import { useGetAllBrand } from "../../../hooks/queries/useGetAllBrands";
+import { useDeleteBrand } from "../../../hooks/mutations/useDeleteBrands";
 
 const ListBrands = () => {
   const [searchText, setSearchText] = useState('');
   const [searchedColumn, setSearchedColumn] = useState('');
   const searchInput = useRef(null);
+  const [messageApi, contextHolder] = message.useMessage();
+  const queryClient = useQueryClient();
+  const { data: brand, isLoading, isError } = useGetAllBrand({
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log(error);
+    }
+  });
 
+  const { mutate } = useDeleteBrand({
+    onSuccess: () => {
+      messageApi.open({
+        type: "success",
+        content: "Thay đổi trạng thái hãng thành công",
+      });
+      queryClient.invalidateQueries({ queryKey: ["get-all-brand"] });
+    },
+    onError(error) {
+      messageApi.open({
+        type: "error",
+        content: error.message,
+      });
+    },
+  })
   const handleSearch = (selectedKeys, confirm, dataIndex) => {
     confirm();
     setSearchText(selectedKeys[0]);
@@ -69,38 +97,15 @@ const ListBrands = () => {
       ),
   });
 
-  const dataSource = [
-    {
-      _id: '1',
-      title: 'Hãng A',
-      status: 'Kích hoạt',
-      isDisabled: false,
-    },
-    {
-      _id: '2',
-      title: 'Hãng B',
-      status: 'Đình chỉ',
-      isDisabled: true,
-    },
-    {
-      _id: '3',
-      title: 'Hãng C',
-      status: 'Kích hoạt',
-      isDisabled: false,
-    },
-    {
-      _id: '4',
-      title: 'Hãng D',
-      status: 'Kích hoạt',
-      isDisabled: false,
-    },
-    {
-      _id: '5',
-      title: 'Hãng E',
-      status: 'Đình chỉ',
-      isDisabled: true,
-    },
-  ];
+  const dataSource = brand?.data.map((item) => {
+    return {
+      key: item.id,
+      _id: item._id,
+      title: item.title,
+      status: item.status === 1 ? "Sử dụng" : "Đình chỉ",
+      isDisabled: item.status !== 1,
+    };
+  });
   const columns = [
     {
       title: "Mã hãng",
@@ -130,15 +135,19 @@ const ListBrands = () => {
       dataIndex: "action",
       width: 250,
       render: (_, brand) => {
+        const isActive = brand.status === "Sử dụng";
         return (
           <div className="flex space-x-3">
             <Popconfirm
-              title={"Kích hoạt hãng?"}
+              title={isActive ? "Đình chỉ hãng?" : "Kích hoạt hãng?"}
+              onConfirm={() => {
+                mutate(brand._id);
+              }}
               okText="Yes"
               cancelText="No"
             >
-              <Button type="primary" >
-                Sử dụng
+              <Button type="primary" style={{ backgroundColor: isActive ? '#ff4d4f' : '#52c41a' }}>
+                {isActive ? "Đình chỉ" : "Sử dụng"}
               </Button>
             </Popconfirm>
             <Button>
@@ -149,9 +158,12 @@ const ListBrands = () => {
       },
     },
   ];
+  if (isLoading) return <p>Loading...</p>
+  if (isError) return <p>Error loading data.</p>
 
   return (
     <div>
+      {contextHolder}
       <div className="flex justify-between items-center mb-5">
         <h1 className="font-semibold text-2xl">Quản lý hãng</h1>
         <Button type="primary">
