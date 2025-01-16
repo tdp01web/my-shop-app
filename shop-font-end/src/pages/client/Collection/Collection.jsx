6092df6cdbbabe247653e47a9551c2f5e838f2d1
Tien Hoang <tienhoangxs122333@gmail.com>
2025-01-16 10:30:09 +0700

    fix data

--- shop-font-end/src/pages/client/Collection/Collection.jsx
+++ shop-font-end/src/pages/client/Collection/Collection.jsx
@@ -32,20 +32,20 @@ const Collection = () => {
   if (isLoading) return <Loader />;
   if (isError)
     return (
-      <div className="w-80% mx-auto ">
+      <div className="mx-auto w-80%">
         Failed to load products. Please try again.
       </div>
     );
   // console.log(products);
 
   return (
-    <div className="relative z-20 flex flex-col gap-6 2xl:w-[80%] 2xl:mx-auto md:px-10">
+    <div className="relative z-20 flex flex-col gap-6 2xl:mx-auto md:px-10 2xl:w-[80%]">
       <img
         src="images/homepage/headblog_banner.webp"
-        className="w-full h-auto rounded-lg shadow-md"
+        className="shadow-md rounded-lg w-full h-auto"
       />
       <div className="mx-auto w-full">
-        <ProductList products={products} />
+        <ProductList products={products.filter((product) => product.status === 1)} />
       </div>
     </div>
   );
